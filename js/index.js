ad986b54e21a7d3dbd81e6151d62bde3cfbb59e2
Erwan <lecren.erwan@gmail.com>
2019-12-05 14:32:51 +0100

    pages done no url only js

--- js/index.js
+++ js/index.js
@@ -6,9 +6,10 @@ let type = 'top'
 let webWorker
 const pages = {
   minPage: 1,
-  currentPage: 3,
+  currentPage: 1,
   maxPage: 1
 }
+let currentSearch = ''
 
 document.getElementById('search-form').addEventListener('submit', (e) => {
   e.preventDefault()
@@ -20,22 +21,62 @@ if (window.Worker) {
   webWorker.onmessage = (event) => {
     movies = type === 'top' ? event.data.movies : event.data.movies.Search
     const totalResults = event.data.movies.totalResults
-    console.log(event.data.movies)
     pages.maxPage = totalResults ? Math.ceil(totalResults / 10) : 1
     displayMovies()
+    if (pages.maxPage > 1) {
+      displayPagesBtns()
+    }
   }
 } else {
   console.log('Not supported by browser')
 }
 
+const displayPagesBtns = () => {
+  const btnContainer = document.getElementById('btn-container')
+  btnContainer.textContent = ''
+  let btns = []
+  if (pages.currentPage - 1 > pages.minPage) {
+    btns = [...btns, createBtn(pages.minPage, 'page-btn page-btn-endpoint')]
+  }
+  if (pages.currentPage - 1 > 0) {
+    btns = [...btns, createBtn(pages.currentPage - 1, 'page-btn')]
+  }
+  btns = [...btns, createBtn(pages.currentPage, 'page-btn page-btn-active')]
+  if (pages.currentPage + 1 <= pages.maxPage) {
+    btns = [...btns, createBtn(pages.currentPage + 1, 'page-btn')]
+  }
+  if (pages.currentPage + 1 < pages.maxPage) {
+    btns = [...btns, createBtn(pages.maxPage, 'page-btn page-btn-endpoint')]
+  }
+  for (const btn of btns) {
+    if (btn.textContent !== pages.currentPage.toString()) {
+      btn.onclick = () => setPage(btn.textContent)
+    }
+    btnContainer.appendChild(btn)
+  }
+}
+
+const setPage = (page) => {
+  pages.currentPage = parseInt(page)
+  searchMovies()
+}
+
+const createBtn = (content, className) => {
+  const currentBtn = document.createElement('div')
+  currentBtn.className = className
+  currentBtn.textContent = content
+  return currentBtn
+}
+
 const searchMovies = () => {
   if (webWorker) {
     const searchInput = document.getElementById('search')
-    const search = searchInput ? searchInput.value : ''
+    pages.currentPage = currentSearch !== searchInput.value ? 1 : pages.currentPage
+    currentSearch = searchInput ? searchInput.value : currentSearch
     const oldType = type
     type = search ? 'search' : 'top'
     if ((type !== oldType) || type === 'search') {
-      webWorker.postMessage({ type: type, search: search, page: pages.currentPage })
+      webWorker.postMessage({ type: type, search: currentSearch, page: pages.currentPage })
     }
   }
 }
