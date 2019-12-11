f530ec5331446b94ec32e4a4510aef5573c4f39a
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-12-11 11:36:55 +0100

    new call api complete

--- js/worker.js
+++ js/worker.js
@@ -1,5 +1,5 @@
 /* eslint-env worker */
-const URL = 'https://developers.themoviedb.org/3/getting-started'
+const URL = 'https://developers.themoviedb.org/3/'
 const API_KEY = '967d5bd6ff00ae4d796d69af5cc03155'
 
 const baseMovies = [
@@ -24,7 +24,8 @@ onmessage = (event) => {
 
 const searchMovies = async (search, page) => {
   let movies = []
-  const response = await fetch(URL + '?apikey=' + API_KEY + '&s=' + search + '&page=' + page)
+  //https://api.themoviedb.org/3/search/movie?api_key=API_KEY%20%3A%20967d5bd6ff00ae4d796d69af5cc03155&query=Star%20Wars&page=1&include_adult=false
+  const response = await fetch(URL + 'search/' + 'movie?api_key=' + API_KEY + '&query=' + search + '&page=' + page + '&include_adult=false')
   const data = await response.json()
   movies = [...movies, data]
   postMessage({ movies: movies[0] })
@@ -32,8 +33,9 @@ const searchMovies = async (search, page) => {
 
 const getTopRatedMovies = async () => {
   let movies = []
+  //https://api.themoviedb.org/3/movie/278?api_key=967d5bd6ff00ae4d796d69af5cc03155
   for (let i = 0; i < baseMovies.length; i++) {
-    const response = await fetch(URL + '?apikey=' + API_KEY + '&t=' + baseMovies[i])
+    const response = await fetch(URL + 'movie' + baseMovies[i] + '?apikey=' + API_KEY)
     const data = await response.json()
     movies = [...movies, data]
   }
