3b8eb3240fdeff99470b57abc3b47f3446c4b34f
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-12-11 12:39:52 +0100

    add movie top rated  url & update the getTopRatedMovies fonction

--- js/worker.js
+++ js/worker.js
@@ -2,27 +2,6 @@
 const URL = 'https://api.themoviedb.org/3/'
 const API_KEY = '967d5bd6ff00ae4d796d69af5cc03155'
 
-const baseMovies = [
-  '278',
-  '238',
-  '155',
-  '389',
-  '122',
-  '680',
-  '429',
-  '550',
-  '120',
-  '13',
-  '27205',
-  '1891',
-  '121',
-  '603',
-  '769',
-  '510',
-  '475557',
-  '807',
-  '637'
-]
 
 onmessage = (event) => {
   if (event.data.type === 'top') {
@@ -38,16 +17,16 @@ const searchMovies = async (search, page) => {
   const response = await fetch(URL + 'search/movie?api_key=' + API_KEY + '&query=' + search + '&page=' + page + '&include_adult=false')
   const data = await response.json()
   movies = [...movies, data]
+  console.log({ movies: movies[0] })
   postMessage({ movies: movies[0] })
 }
 
 const getTopRatedMovies = async () => {
   let movies = []
-  //https://api.themoviedb.org/3/movie/278?api_key=967d5bd6ff00ae4d796d69af5cc03155
-  for (let i = 0; i < baseMovies.length; i++) {
-    const response = await fetch(URL + 'movie/' + baseMovies[i] + '?api_key=' + API_KEY)
-    const data = await response.json()
-    movies = [...movies, data]
-  }
-  postMessage({ movies: movies })
+  //https://api.themoviedb.org/3/movie/top_rated?api_key=967d5bd6ff00ae4d796d69af5cc03155&page=1
+  const response = await fetch(URL + 'movie/top_rated?api_key=' + API_KEY + '&page=1')
+  const data = await response.json()
+  movies = [...movies, data]
+  postMessage({ movies: movies[0] })
 }
+
