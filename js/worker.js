b2de80c4968c7654aeca12ae7862970587608168
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-12-11 11:34:15 +0100

    new call api complete

--- js/worker.js
+++ js/worker.js
@@ -3,16 +3,15 @@ const URL = 'https://developers.themoviedb.org/3/getting-started'
 const API_KEY = '967d5bd6ff00ae4d796d69af5cc03155'
 
 const baseMovies = [
-  'The Shawshank Redemption',
-  'The Godfather',
-  'The Dark Knight',
-  '12 Angry Men',
-  'The Lord of the Rings: The Return of the King',
-  'Pulp Fiction',
-  'The Good, the Bad and the Ugly',
-  'Fight Club',
-  'The Lord of the Rings: The Fellowship of the Ring',
-  'Forrest Gump'
+  '278',
+  '238',
+  '155',
+  '389',
+  '122',
+  '680',
+  '429',
+  '550',
+  '120'
 ]
 
 onmessage = (event) => {
